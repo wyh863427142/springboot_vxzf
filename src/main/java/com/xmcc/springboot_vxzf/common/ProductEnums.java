e50b20c31c5edd3ba789a854b3f087924e4c48f2
wyh863427142 <wyh863427142@163.com>
2019-04-22 15:08:06 +0800

    第四次提交：     今天进行了订单开发

--- src/main/java/com/xmcc/springboot_vxzf/common/ProductEnums.java
+++ src/main/java/com/xmcc/springboot_vxzf/common/ProductEnums.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public enum ProductEnums {
     PRODUCT_NOT_ENOUGH(1,"商品库存不足");
-
+    //。。。。。。
     private int code;
     private String meg;
     ProductEnums(int code,String msg){
